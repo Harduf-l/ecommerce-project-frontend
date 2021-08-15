cbb5a32cdbdb4887770360c4efc08f18a6bcf2ec
harduf laor <harduf93@gmail.com>
2021-08-15 22:21:08 +0300

    inital commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,7 @@
+let Content = () => {
+    return (
+        <p>page content</p>
+    )
+}
+
+export default Content 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+let Footer = () => {
+    return (
+        <div style={footerStyle}>
+        <p>footer</p>
+        </div>
+    )
+}
+
+export default Footer 
+
+let footerStyle = {
+    backgroundColor: "grey", 
+}
