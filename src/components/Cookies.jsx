deefd6f48d90ffe69df0c1002010e4de02ddd77b
harduf laor <harduf93@gmail.com>
2021-08-16 21:38:29 +0300

    creating cookie component

---
+++ src/components/Cookies.jsx
@@ -0,0 +1,51 @@
+let Cookies = ({foodContent, myid, plus, minus}) => {
+    let {header, description, moreDescription, loveIt, price, pic1, pic2, pic3, quantity} = foodContent
+    console.log(header, )
+    return(
+        <div id="bigfoodDiv" className="d-flex justify-content-center flex-wrap">
+         
+    <div className="me-5 mt-5 p-5">
+        <h4 style={{marginBottom: "20px"}}>{header}</h4>
+        <div>{description}</div>
+        <div>{moreDescription}</div>
+        <p style={{color: "#6f0000" , marginTop: "20px"}}>{loveIt}<span><i className="fas fa-heart"></i></span></p>
+        <div>מחיר: {price} ש"ח</div>
+        <br/>
+        <p><span style={{marginLeft: "10px"}}>כמות: {quantity}</span> 
+        <span><button onClick={() => plus(myid) } style={{backgroundColor: "#2e4e14", color:"white"}} class="btn btn-sm">+</button></span> 
+        <span><button  onClick={() => minus(myid) }  style={{backgroundColor: "#2e4e14", marginRight: "10px"}} class="btn btn-success btn-sm">-</button></span></p>
+        <button className="btn btn-secondary btn-sm mb-1 mt-2">הוסף לעגלה</button>
+    </div>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style={{width: "400px"}}>
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={pic1} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={pic2} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={pic3} class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+</div>
+    )
+}
+
+export default Cookies;
